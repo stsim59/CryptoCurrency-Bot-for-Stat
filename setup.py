599a685273feadf2a549da62bb76a2bc60caa40a
Simard <56772506+stsim59@users.noreply.github.com>
2019-10-28 08:11:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ Created on Thu Oct 24 13:08:36 2019
 
 import binance as bi
 import markets_op as ma
+from pandas import Series, DataFrame
 
 api_key = ''
 api_secret = ''
 client = bi.Client(api_key, api_secret)
 
-markets = ma.get_markets_list()
-df_market = ma.get_markets_bs_balance(markets)
+markets = ma.get_markets_list(client)
+df_market = ma.market_book(markets, symbol='BTC')
 
 print(df_market)
