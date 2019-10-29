c0c9c90991175eccce60eafc23c5e3d95b867494
Steve Simard <stevesimard@mailoo.org>
2019-10-29 15:51:37 -0400

    Simple wallet 1

--- setup.py
+++ setup.py
@@ -8,13 +8,18 @@ Created on Thu Oct 24 13:08:36 2019
 
 import binance as bi
 import markets_op as mo
-from pandas import Series, DataFrame
+import trade as tr
+#from pandas import Series, DataFrame
 
 api_key = ''
 api_secret = ''
 client = bi.Client(api_key, api_secret)
 
-markets = mo.get_markets_list(client)
-df_market = mo.balances_to_dataframe(client, markets)
+wallet1 = tr.simple_wallet(ETH=10, BTC=0)
+wallet2 = tr.simple_wallet(ETH=10, BTC=0)
+tr.trade(client, wallet1)
 
-print(df_market)
+def get_markets_to_dataframe():
+    markets = mo.get_markets_list(client)
+    df_market = mo.balances_to_dataframe(client, markets)
+    print(df_market)
---
+++ trade.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import time
+import markets_op
+
+def trade(client, wallet, buy_pc=1.02, sell_pc=0.98):
+    
+    while True:
+        book = markets_op.market_book(client, symbol='ETHUSDT', limit=1000)
+        print('Looking', book.symbol, ', balance are', book.balance_pc)
+    
+        if book.balance_pc[0] > buy_pc:
+            if wallet.balance['ETH'] == 0:
+                print('Balance (', book.balance_pc[0], ') above limit, buying ETH with', wallet.balance['USDT'], 'USDT, at', book.mid_price, 'ETH')
+                wallet.new_order('ETHUSDT', 'buy', wallet.balance['USDT'], book.mid_price)
+        if book.balance_pc[0] < sell_pc:
+            if wallet.balance['ETH'] > 0:
+                print('Balance (', book.balance_pc[0], ') below limit, selling ETH with', wallet.balance['USDT'], 'USDT, at', book.mid_price, 'ETH')
+                wallet.new_order('ETHUSDT', 'sell', wallet.balance['ETH'], book.mid_price)
+        time.sleep(30)
+
+        
+class simple_wallet:
+   
+    def __init__(self, BTC=10, USDT=0, stoploss=0.05, **tickers):
+        self.balance = {}
+        self.balance["BTC"] = BTC
+        self.balance["USDT"] = USDT
+        for ticker, value in tickers.items():
+            self.balance[ticker] = value
+        self.stoploss = stoploss
+        self.total_fees = 0
+            
+    def get_balance(self):
+        return self.balance
+    
+    def new_order(self, symbol, side, quantity, price):
+        #full list : (self, symbol, side, order_type, price, quantity, timestamp):
+        #timestamp : number of millisecond before the server reject the request. Serious trading is about timing. It's 
+        #recommanded to use a small windows (5000 or less), max is 60 000.
+        self.total_fees += quantity*price*0.001
+        if side == "buy":
+            self.balance[symbol[:3]] += quantity/price*0.999   #currency
+            self.balance[symbol[-4:]] -= quantity   #USDT
+        elif side == "sell":
+            self.balance[symbol[:3]] -= quantity   #currency
+            self.balance[symbol[-4:]] += quantity*price*0.999   #USDT
+        else:
+            raise "Buy or sell non-defined"
+    
+    def total_value_btc(self):
+        total = 0
+        for ticker, value in self.balance.items():
+            if ticker == 'BTC':
+                total += value
+            else:
+                ticker_info = client.get_symbol_ticker(symbol=(ticker + 'BTC'))
+                total += float(ticker_info['price'])*value
+        return total
+    
+    def total_value_usd(self):
+        return get_btc_usd() * self.total_value_btc()
